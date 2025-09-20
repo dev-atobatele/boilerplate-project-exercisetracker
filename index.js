b8dae5db8b5e517d26fbce07485114711f13cecb
dev-atob <dev.atobatele@gmail.com>
2025-09-20 00:45:22 -0400

    Rolledback to 87% passing tests

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-const bodyParser = require('body-parser')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
-
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
 
 app.use(bodyParser.urlencoded({
   extended:false
@@ -16,9 +5,10 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 let users = [];
-function getUser(_id) {
+let id = 1;
+function getUser(id) {
   for (const user of users) {
-    if (user._id==_id) {
+    if (user._id==id) {
       return user;
     }
   }
@@ -30,14 +20,15 @@ app.route('/api/users')
   res.json(users);
 })
 .post(function postUser(req,res) {
-  let body = req.body.username;
-  let User = {
-    username: body.slice(0,body.lastIndexOf('_')),
-    _id: body.slice(body.lastIndexOf('_')+1),
+  let user = {
+    username: `${req.body.username}`,
+    _id:`${id}`,
+    excercises:[]
   };
   //console.log(user);
-  res.json(User);
-  users.push(User);
+  res.json(user);
+  id+=1;
+  users.push(user);
 });
 
 app.post('/api/users/:_id/exercises',
@@ -45,37 +36,27 @@ function postExcercise(req,res) {
   let body = req.body;
   let {_id} = req.params;
   let user = getUser(_id);
-  let Exercise = {
-    ...user,
+  let excercise = {
     description:body.description,
     duration:Number(body.duration),
     date:new Date(body.date).toDateString()
   };
   if (!body.date) {
-    Exercise.date = new Date().toDateString();
+    excercise.date = new Date().toDateString();
   };
-  if (!user.exs) {
-    user.exs = [];
-  };
-  user.exs.push(Exercise);
-  //console.log(Exercise);
-  res.send(Exercise);
+  user.excercises.push(excercise);
+  console.log(user);
+  res.json(user);
 });
 
 app.get('/api/users/:_id/logs',
 function getLogs(req,res){
   let {_id} = req.params;
   let user = getUser(_id);
-  //console.log(user)
-  let Log = {
-    ...user,
-    count:user.exs.length,
-    log:user.exs
-  };
-  //console.log(Log);
-  res.send(Log);
+  let logs = {
+    count : user.excercises.length,
+    log : user.excercises
+  }
+  console.log(logs);
+  res.json(logs);
 });
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
